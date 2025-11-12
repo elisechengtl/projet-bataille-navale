c2776cd2bf21a3782f4ee56169b7e0f12d6c7f56
elisechengtl <tianle.cheng@centrale-med.fr>
2025-11-12 21:58:14 +0100

    Update test_grille.py
    Test du constructeur et de la méthode

--- tests/test_grille.py
+++ tests/test_grille.py
@@ -1,5 +1,14 @@
 from grille import Grille
 
 def test_init():
-    grille = Grille()
-    assert isinstance(grille, Grille)
+    g = Grille(5, 8)
+    assert g.nombre_lignes == 5
+    assert g.nombre_colonnes == 8
+    assert len(g.matrice) == 5 * 8
+    assert all(c == g.vide for c in g.matrice)
+
+def test_tirer():
+    g = Grille(5, 8)
+    g.tirer(2, 3)
+    index = 2 * g.nombre_colonnes + 3
+    assert g.matrice[index] == g.touche
