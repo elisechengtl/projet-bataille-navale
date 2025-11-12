963828b6e4c9273a00fc5875765c40696244485f
elisechengtl <tianle.cheng@centrale-med.fr>
2025-11-12 21:55:33 +0100

    Update grille.py
    Ajout constructeur et méthode tirer(ligne, colonne)

--- grille.py
+++ grille.py
@@ -1,2 +1,16 @@
 class Grille:
-  pass
+    def __init__(self, nombre_lignes, nombre_colonnes):
+        """Crée une grille vide avec les dimensions données."""
+        self.nombre_lignes = nombre_lignes
+        self.nombre_colonnes = nombre_colonnes
+        self.vide = '∿' 
+        self.touche = 'x' 
+        self.matrice = [self.vide] * (nombre_lignes * nombre_colonnes)
+
+    def tirer(self, ligne, colonne):
+        """Marque un tir sur la grille à la position."""
+        if 0 <= ligne < self.nombre_lignes and 0 <= colonne < self.nombre_colonnes:
+            index = ligne * self.nombre_colonnes + colonne
+            self.matrice[index] = self.touche
+        else:
+            print("Coordonnées invalides")
